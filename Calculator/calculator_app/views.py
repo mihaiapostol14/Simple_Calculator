53aab1c40d278b81e165fb0a346f143fe617d8b5
Mihai Apostol <mihaiapostol153@gmail.com>
2025-01-13 10:42:30 +0200

    Change from function calculator to class CalculatorView

--- Calculator/calculator_app/views.py
+++ Calculator/calculator_app/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+
+from .utils import CalculatorMixin
 
 
-def calculator(request) -> render:
-    context = {
-        'title': 'Calculator'
-    }
-    return render(request=request, template_name='calculator_app/calculator.html',context=context)
+class CalculatorView(CalculatorMixin, TemplateView):
+    template_name = 'calculator_app/calculator.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return self.get_mixin_context(context=context, title='Calculator')
