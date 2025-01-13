a894d5bbfff4f284845aca9f263d44f17665dec1
Mihai Apostol <mihaiapostol153@gmail.com>
2025-01-13 12:56:53 +0200

    register new app calculator_app2

--- Calculator/Calculator/settings.py
+++ Calculator/Calculator/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'calculator_app.apps.CalculatorAppConfig',
+    'calculator_app2.apps.CalculatorApp2Config',
 ]
 
 MIDDLEWARE = [
--- Calculator/Calculator/urls.py
+++ Calculator/Calculator/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('calculator_app.urls')),
+    path('calculator_app2',include('calculator_app2.urls')),
 ]
